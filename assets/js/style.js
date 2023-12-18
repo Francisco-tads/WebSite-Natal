475435288e0b4df30b9d9407a941e82e34d7165e
Francisco vieira <francisco.vieiratads@gmail.com>
2023-12-18 19:57:14 -0300

    modificação na style.js

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,14 +4,14 @@ const navMenu = document.getElementById('nav'),
     navClose = document.getElementById('nav-close')
 
 /* MENU SHOW */
-if(navToggle){
+if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 /* MENU HIDDEN */
-if(navClose){
+if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
@@ -20,7 +20,7 @@ if(navClose){
 /*========= REMOVER MENU MOBILE ==========================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav')
     //Quando o link for clicado a class show-menu será removido
     navMenu.classList.remove('show-menu')
@@ -28,12 +28,12 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*========= BOX SHADOW NO HEADER =========================*/
-function scrollHeader(){
+function scrollHeader() {
     const scrollHeader = document.getElementById('header')
 
-    if(this.scrollY >= 80){
+    if (this.scrollY >= 80) {
         scrollHeader.classList.add('scroll-header')
-    }else{
+    } else {
         scrollHeader.classList.remove('scroll-header')
     }
 }
@@ -46,13 +46,13 @@ const scrollActive = () => {
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 160,
-        sectionId = current.getAttribute('id'),
-        sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+            sectionTop = current.offsetTop - 160,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-        if(scrolly > sectionTop && scrolly <= sectionTop + sectionHeight){
+        if (scrolly > sectionTop && scrolly <= sectionTop + sectionHeight) {
             sectionClass.classList.add('active-link')
-        }else{
+        } else {
             sectionClass.classList.remove('active-link')
         }
     })
@@ -67,25 +67,26 @@ const daysContainer = document.querySelector('#days')
 const loadingContainer = document.querySelector('#loading')
 const countdownContainer = document.querySelector('#countdown')
 
-const eventDate = new Date('December 25 2022 00:00:00')/**inserir a data do final do evento */
+    /**inserir a data do final do evento */
+const eventDate = new Date('December 25 2023 00:00:00')
 
-const updateCountdown = () =>{
-  const currenTime = new Date()
-  const difference = eventDate - currenTime
-  const days = Math.floor(difference / 1000 / 60 / 60 / 24)
-  const hours = Math.floor(difference / 1000 / 60 / 60 ) % 24
-  const minutes = Math.floor(difference / 1000 / 60 ) % 60
-  const seconds = Math.floor(difference / 1000 ) % 60
+const updateCountdown = () => {
+    const currenTime = new Date()
+    const difference = eventDate - currenTime
+    const days = Math.floor(difference / 1000 / 60 / 60 / 24)
+    const hours = Math.floor(difference / 1000 / 60 / 60) % 24
+    const minutes = Math.floor(difference / 1000 / 60) % 60
+    const seconds = Math.floor(difference / 1000) % 60
 
-  secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
-  minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
-  hoursContainer.textContent = hours < 10 ? '0' + hours : hours
-  daysContainer.textContent = days < 10 ? '0' + days : days
+    secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
+    minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
+    hoursContainer.textContent = hours < 10 ? '0' + hours : hours
+    daysContainer.textContent = days < 10 ? '0' + days : days
 }
 
-setTimeout( () =>{
-  loadingContainer.remove()
-  countdownContainer.style.display = 'flex'
+setTimeout(() => {
+    loadingContainer.remove()
+    countdownContainer.style.display = 'flex'
 }, 100)
 
 setInterval(updateCountdown, 1000)
@@ -99,7 +100,7 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__content, .section__title, .contact__content`)
-sr.reveal(`.section__subtitle`, {interval: 100})
-sr.reveal(`.hero__img, .about__img, .donate__title, .discount__img`, {origin: 'left'})
-sr.reveal(`.about__content, .donate__img, .discount__content`, {origin: 'right'})
-sr.reveal(`.category, .accessory__cards, .contact__form`, {origin: 'bottom'})
+sr.reveal(`.section__subtitle`, { interval: 100 })
+sr.reveal(`.hero__img, .about__img, .donate__title, .discount__img`, { origin: 'left' })
+sr.reveal(`.about__content, .donate__img, .discount__content`, { origin: 'right' })
+sr.reveal(`.category, .accessory__cards, .contact__form`, { origin: 'bottom' })
